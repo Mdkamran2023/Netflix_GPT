ea44ff161c1c4e51595064497bcddd32bee44e23
Kamran <mdkamran0109@gmail.com>
2024-03-07 00:20:08 +0530

    modify

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,9 +2,9 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
-  console.log(movies);
+  // console.log(movies);
   return (
-    <div className="px-6">
+    <div className="px-6 max-sm:px-0">
       <h1 className=" text-lg md:text-2xl font-normal py-4 text-white">{title}</h1>
       <div className=" flex whitespace-nowrap overflow-x-auto no-scrollbar scroll-smooth overflow-y-hidden">
         <div className="flex">
